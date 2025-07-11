8e9c75c0a28049be0b0ccf7d8551beb3032b17eb
Ho Tuan Khanh <hotuankhanh20112016@gmail.com>
2025-07-12 02:32:40 +0700

    fix error

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
-
 export default function GlobalError({ error, reset }: { error: Error; reset: () => void }) {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
-
   return (
-    <div>
-      <h2>Đã có lỗi xảy ra!</h2>
-      <p>{error.message}</p>
-      <button onClick={() => reset()}>Thử lại</button>
-    </div>
+    <html>
+      <body>
+        <h2>Đã có lỗi xảy ra!</h2>
+        <p>{error.message}</p>
+        <button onClick={() => reset()}>Thử lại</button>
+      </body>
+    </html>
   );
 }
